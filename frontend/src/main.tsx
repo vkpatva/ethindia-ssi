caa6e03b324e37bd5ce9931c7e26712021312c1a
Viraj Patva <viraj.patva@smartsensesolutions.com>
2023-12-08 23:13:43 +0530

    toast containter , axios and other pages added , main file changed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ToastContainer } from "react-toastify";
+import "./styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+const activeChain = "mumbai";
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <ThirdwebProvider
+      clientId={import.meta.env.VITE_THIRDWEB_APP_ID}
+      activeChain={activeChain}
+    >
+      <App />
+      <ToastContainer />
+    </ThirdwebProvider>
+  </BrowserRouter>
+);
