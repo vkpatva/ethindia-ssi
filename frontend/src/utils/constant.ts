83e4e0b9df75962def142ca906ece2377ed50e76
Viraj Patva <viraj.patva@smartsensesolutions.com>
2023-12-10 01:47:10 +0530

    Initialize subgraph

--- frontend/src/utils/constant.ts
+++ frontend/src/utils/constant.ts
@@ -2,6 +2,6 @@ export const credentialAccountFactory =
   "0xFb8fEFb2EDCBf9850AEEEA24a32B575494715421";
 
 export const CredentialEventService =
-  "0xb7aabf80f1cec5c54c286329330bc608bac09f39";
+  "0xFccFd24c6742cF24F8e06b697c4E7d787C8f10Aa";
 export const ClaimToken = "0x6d6Bf337A4EaB886420860d41Ec51dE1c31B8d11";
-export const SubId = 1137;
+export const SubId = 1161;
